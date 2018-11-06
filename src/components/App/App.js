5f350b6517bf7b709c307671c5c000dcfb3b6df2
Rocio <rocio.cejudo.soronellas@gmail.com>
2018-11-06 15:48:06 +0100

    deploy2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ import NotFound from '../elements/NotFound/NotFound';
 
 const App =() =>{
     return(
-        <BrowserRouter basename='/Reactmovie2/'>
+        <BrowserRouter>
             <React.Fragment>
                 <Header />
                 <Switch>
