bad0b6281453775e0b4047508f0c2b35ce6c3c91
rcellas <rocio.cejudo.soronellas@gmail.com>
2018-11-06 15:50:55 +0100

    Updates

---
+++ precache-manifest.0859a045479687b6485222968f603722.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "102c2cfc808051ab2368",
+    "url": "/Reactmovie2/static/js/runtime~main.102c2cfc.js"
+  },
+  {
+    "revision": "f5180f8af4d271d148c3",
+    "url": "/Reactmovie2/static/js/main.f5180f8a.chunk.js"
+  },
+  {
+    "revision": "48d991aefd64a92a8d6a",
+    "url": "/Reactmovie2/static/js/1.48d991ae.chunk.js"
+  },
+  {
+    "revision": "f5180f8af4d271d148c3",
+    "url": "/Reactmovie2/static/css/main.8b4e1181.chunk.css"
+  },
+  {
+    "revision": "01e46d2bafaaa3cff6515aea3702bb9e",
+    "url": "/Reactmovie2/index.html"
+  }
+];
